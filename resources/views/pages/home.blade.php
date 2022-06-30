27fbc861fbcd55237d5563836fe562e61d7ae711
germanusK <germanuskeming@gmail.com>
2022-06-30 22:02:37 +0100

    home page proper former design

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,97 +1,97 @@
 @extends('templates.page-template')
 @section('page-content')
-<div class="w-full py-1">
-    <div class="w-11/12 mx-auto py-1 flex h-fit items-center align-middle">
-        <div class="w-7/12 px-4 flex items-center justify-center align-middle h-fit">
-            <img src="{{asset('img/bgcode.png')}}" alt="" class="w-96 max-w-full my-8 mx-4 block">
-        </div>
-        <div class="w-5/12 px-3 flex items-center tracking-widest leading-relaxed align-middle justify-center h-full text-4xl text-red-500 font-semibold font-sans">
+<div class="w-full home-bg">
+    <div class="w-full mx-auto py-1 flex items-center align-middle hl1">
+        <div class="w-full md:w-4/5 text-center mx-auto px-3  items-center tracking-widest leading-relaxed align-middle justify-center h-full text-3xl text-d font-semibold font-sans flex flex-wrap flex-col">
+            <div class="w-full md:w-4/5 text-center mx-auto px-3 tracking-widest leading-relaxed font-bold mt-3 text-5xl font-sans block">Welcome to Sonsman</div>
             Join us where real things are happening
         </div>
     </div>
-    <div class="w-11/12 mx-auto py-10 my-1 md:flex items-center align-middle">
-        <div class="md:w-5/12 text-center text-4xl text-blue-500 font-semibold">What we do</div>
+    <div class="w-full mx-auto py-10 md:flex items-center align-middle bg-white bg-opacity-10">
+        <div class="md:w-5/12 text-center text-4xl text-d font-semibold">What we do</div>
         <div class="md:w-7/12 sm:flex flex-wrap justify-between">
-            <div class="sm:w-5/12 mx-auto py-7 px-3 my-4 flex flex-wrap items-center align-middle justify-center border border-blue-200 hover:border-blue-500 rounded-xl">
-                <span class="fas fa-code text-5xl font-black text-red-500"></span>
-                <div class="text-2xl sm:text-3xl w-full block text-center my-4 text-blue-500 font-semibold mx-auto capitalize">Web & Mobile development</div>
+            <div class="sm:w-5/12 mx-auto py-7 px-3 my-4 flex flex-wrap items-center align-middle justify-center bg-p bg-opacity-50 border border-d hover:border-blue-500 rounded-xl">
+                <span class="fas fa-code text-5xl font-black text-s"></span>
+                <div class="text-2xl sm:text-3xl w-full block text-center my-4 text-d font-semibold mx-auto capitalize">Web & Mobile development</div>
             </div>
-            <div class="sm:w-5/12 mx-auto py-7 px-3 my-4 flex flex-wrap items-center align-middle justify-center border border-blue-200 hover:border-blue-500 rounded-xl">
-                <span class="fab fa-deviantart text-5xl font-black text-red-500"></span>
-                <div class="text-2xl sm:text-3xl w-full block text-center my-4 text-blue-500 font-semibold mx-auto">Web Design</div>
+            <div class="sm:w-5/12 mx-auto py-7 px-3 my-4 flex flex-wrap items-center align-middle justify-center bg-p border border-d hover:border-blue-500 rounded-xl">
+                <span class="fab fa-deviantart text-5xl font-black text-s"></span>
+                <div class="text-2xl sm:text-3xl w-full block text-center my-4 text-d font-semibold mx-auto">Web Design</div>
             </div>
-            <div class="sm:w-5/12 mx-auto py-7 px-3 my-4 flex flex-wrap items-center align-middle justify-center border border-blue-200 hover:border-blue-500 rounded-xl">
-                <span class="fas fa-dice text-5xl font-black text-red-500"></span>
-                <div class="text-2xl sm:text-3xl w-full block text-center my-4 text-blue-500 font-semibold mx-auto">Graphics Design</div>
+            <div class="sm:w-5/12 mx-auto py-7 px-3 my-4 flex flex-wrap items-center align-middle justify-center bg-p border border-d hover:border-blue-500 rounded-xl">
+                <span class="fas fa-dice text-5xl font-black text-s"></span>
+                <div class="text-2xl sm:text-3xl w-full block text-center my-4 text-d font-semibold mx-auto">Graphics Design</div>
             </div>
-            <div class="sm:w-5/12 mx-auto py-7 px-3 my-4 flex flex-wrap items-center align-middle justify-center border border-blue-200 hover:border-blue-500 rounded-xl">
-                <span class="fas fa-laugh-beam text-5xl font-black text-red-500"></span>
-                <div class="text-2xl sm:text-3xl w-full block text-center my-4 text-blue-500 font-semibold mx-auto">Sonsman Quotes</div>
+            <div class="sm:w-5/12 mx-auto py-7 px-3 my-4 flex flex-wrap items-center align-middle justify-center bg-p border border-d hover:border-blue-500 rounded-xl">
+                <span class="fas fa-laugh-beam text-5xl font-black text-s"></span>
+                <div class="text-2xl sm:text-3xl w-full block text-center my-4 text-d font-semibold mx-auto">Sonsman Quotes</div>
             </div>
         </div>
     </div>
-    <div class="w-full bg-blue-800 bg-opacity-90 bgd1 py-1">
+
+    <!-- Apply some animations here -->
+    <div class="w-full bg-p bg-opacity-90 bgd1 py-1">
         <div class="w-11/12 mx-auto my-12 px-10  md:h-96 relative">
-            <div class="w-fit px-3 py-2 md:absolute left-10 top-1/4 text-white italic text-3xl max-w-xs">Its better when it talks than quiet </div>
+            <div class="w-fit px-3 py-2 md:absolute left-10 top-1/4 text-white italic text-3xl max-w-xs">It can speak for itself. Its Express </div>
             <div class="w-fit px-3 py-2 md:absolute right-10 top-10 text-white italic text-3xl max-w-xs">when your thought changes into uptimized designs</div>
             <div class="w-fit px-3 py-2 md:absolute left-1/4 bottom-1/3 text-white italic text-3xl max-w-xs">Trying to give my app a sense of perception</div>
             <div class="w-fit px-3 py-2 md:absolute left-2/4 bottom-0 text-white italic text-3xl max-w-xs">Do you need a logo that talks?</div>
         </div>
     </div>
-    <div class="w-11/12 mx-auto py-8">
-        <div class="w-full flex items-end justify-end text-4xl text-blue-500 font-semibold">Sonsman accepts it this way</div>
+    <div class="w-full mx-auto py-8 bg-d">
+        <div class="w-full flex items-end justify-end text-4xl text-p font-semibold px-12">How we understand it</div>
         <div class="w-full sm:flex flex-wrap justify-around py-4">
-            <div class="w-1/2 md:w-1/4 mx-auto my-5 p-2 text-center text-4xl font-bold hover:text-blue-500 rounded-md capitalize">
-                <div class="w-full h-full rounded-md py-10 px-2 border border-blue-300 flex items-center align-middle">access s-quote services</div>
+            <div class="w-1/2 md:w-1/4 mx-auto my-5 p-2 text-center text-4xl font-bold bg-s hover:text-blue-500 rounded-md capitalize">
+                <div class="w-full h-full rounded-md py-10 px-2 border border-p bg-p flex items-center align-middle text-d">access s-quote services</div>
             </div>
-            <div class="w-1/2 md:w-1/4 mx-auto my-5 p-2 text-center text-4xl font-bold hover:text-blue-500 rounded-md capitalize">
-                <div class="w-full h-full rounded-md py-10 px-2 border border-blue-300 flex items-center align-middle">Web development services</div>
+            <div class="w-1/2 md:w-1/4 mx-auto my-5 p-2 text-center text-4xl font-bold bg-s hover:text-blue-500 rounded-md capitalize">
+                <div class="w-full h-full rounded-md py-10 px-2 border border-p bg-p flex items-center align-middle text-d">Web development services</div>
             </div>
-            <div class="w-1/2 md:w-1/4 mx-auto my-5 p-2 text-center text-4xl font-bold hover:text-blue-500 rounded-md capitalize">
-                <div class="w-full h-full rounded-md py-10 px-2 border border-blue-300 flex items-center align-middle">Graphics design services</div>
+            <div class="w-1/2 md:w-1/4 mx-auto my-5 p-2 text-center text-4xl font-bold bg-s hover:text-blue-500 rounded-md capitalize">
+                <div class="w-full h-full rounded-md py-10 px-2 border border-p bg-p flex items-center align-middle text-d">Graphics design services</div>
             </div>
         </div>
     </div>
-    <div class="w-11/12 mx-auto py-12 my-8 h-fit rounded-r-full rounded-l-full bg-white">
-        <div class="w-1/2 mx-auto h-96 py-2 rounded-l-full rounded-r-full shadow-2xl shadow-red-500 bg-white relative flex align-middle items-center justify-center">
-            <div class="text-blue-500 rounded-full text-4xl font-black text-center">Who benefits</div>
-            <p class="sm:w-1/2 absolute bottom-3/4 right-3/4 text-2xl text-blue-500 font-semibold capitalize py-4 px-6 rounded-r-full rounded-l-full bg-white shadow-xl shadow-red-500 text-center">Business & Institution owners</p>
-            <p class="sm:w-1/2 absolute top-3/4 right-3/4 text-2xl text-blue-500 font-semibold capitalize py-4 px-6 rounded-r-full rounded-l-full bg-white shadow-xl shadow-red-500 text-center">Software project managers</p>
-            <p class="sm:w-1/2 absolute bottom-3/4 left-3/4 text-2xl text-blue-500 font-semibold capitalize py-4 px-6 rounded-r-full rounded-l-full bg-white shadow-xl shadow-red-500 text-center">Quote lovers and users</p>
+    <div class="w-full mx-auto py-20 h-fit">
+        <div class="w-1/2 mx-auto h-96 py-2 rounded-l-full rounded-r-full shadow-2xl border-b-4 border-s shadow-red-600 bg-d relative flex align-middle items-center justify-center">
+            <div class="text-p rounded-full text-4xl font-black text-center uppercase">Who benefits</div>
+            <p class="sm:w-1/2 absolute bottom-3/4 right-3/4 text-2xl text-p font-semibold capitalize py-4 px-6 rounded-r-full rounded-l-full bg-d shadow-xl shadow-red-500 border-b-4 border-s text-center">Business & Institution owners</p>
+            <p class="sm:w-1/2 absolute top-3/4 right-3/4 text-2xl text-p font-semibold capitalize py-4 px-6 rounded-r-full rounded-l-full bg-d shadow-xl shadow-red-500 border-b-4 border-s text-center">Software project managers</p>
+            <p class="sm:w-1/2 absolute bottom-3/4 left-3/4 text-2xl text-p font-semibold capitalize py-4 px-6 rounded-r-full rounded-l-full bg-d shadow-xl shadow-red-500 border-b-4 border-s text-center">Quote lovers and users</p>
         </div>
     </div>
-    <div class="w-full py-12">
+    <div class="w-full py-12 bg-d">
         <div class="w-11/12 mx-auto md:h-96 flex flex-col items-center align-middle justify-center">
-            <div class="w-full text-center text-4xl text-stone-800 py-6 capitalize font-black">We set an example</div>
+            <div class="w-full text-center text-4xl text-p py-6 capitalize font-black">Our products</div>
             <div class="w-full h-fit overflow-x-scroll whitespace-nowrap mini-scrollbar">
                 <div class="w-fit h-64 flex flex-row justify-around">
-                    <div class="w-60 h-60 mx-4 rounded border border-blue-200 border-opacity-30 flex flex-col align-top items-center justify-start">
-                        <img src="{{asset('img/bgd1.png')}}" alt="" class="w-full h-5/6 rounded mb-2 mt-0">
-                        <div class="mx-2 text-center text-xl text-stone-500 font-semibold capitalize">Product1</div>
+                    <div class="w-60 h-60 mx-4 rounded border-y-2 bg-p border-s border-double border-opacity-30 flex flex-col align-top items-center justify-start">
+                        <img src="{{asset('img/sunny.jpg')}}" alt="" class="w-full h-5/6 rounded mb-2 mt-0">
+                        <div class="mx-2 text-center text-xl text-s font-semibold capitalize">Product1</div>
                     </div>
-                    <div class="w-60 h-60 mx-4 rounded border border-blue-200 border-opacity-30 flex flex-col align-top items-center justify-start">
-                        <img src="{{asset('img/bgd1.png')}}" alt="" class="w-full h-5/6 rounded mb-2 mt-0">
-                        <div class="mx-2 text-center text-xl text-stone-500 font-semibold capitalize">Product1</div>
+                    <div class="w-60 h-60 mx-4 rounded border-y-2 bg-p border-s border-double border-opacity-30 flex flex-col align-top items-center justify-start">
+                        <img src="{{asset('img/sunny.jpg')}}" alt="" class="w-full h-5/6 rounded mb-2 mt-0">
+                        <div class="mx-2 text-center text-xl text-s font-semibold capitalize">Product1</div>
                     </div>
-                    <div class="w-60 h-60 mx-4 rounded border border-blue-200 border-opacity-30 flex flex-col align-top items-center justify-start">
-                        <img src="{{asset('img/bgd1.png')}}" alt="" class="w-full h-5/6 rounded mb-2 mt-0">
-                        <div class="mx-2 text-center text-xl text-stone-500 font-semibold capitalize">Product1</div>
+                    <div class="w-60 h-60 mx-4 rounded border-y-2 bg-p border-s border-double border-opacity-30 flex flex-col align-top items-center justify-start">
+                        <img src="{{asset('img/sunny.jpg')}}" alt="" class="w-full h-5/6 rounded mb-2 mt-0">
+                        <div class="mx-2 text-center text-xl text-s font-semibold capitalize">Product1</div>
                     </div>
-                    <div class="w-60 h-60 mx-4 rounded border border-blue-200 border-opacity-30 flex flex-col align-top items-center justify-start">
-                        <img src="{{asset('img/bgd1.png')}}" alt="" class="w-full h-5/6 rounded mb-2 mt-0">
-                        <div class="mx-2 text-center text-xl text-stone-500 font-semibold capitalize">Product1</div>
+                    <div class="w-60 h-60 mx-4 rounded border-y-2 bg-p border-s border-double border-opacity-30 flex flex-col align-top items-center justify-start">
+                        <img src="{{asset('img/sunny.jpg')}}" alt="" class="w-full h-5/6 rounded mb-2 mt-0">
+                        <div class="mx-2 text-center text-xl text-s font-semibold capitalize">Product1</div>
                     </div>
-                    <div class="w-60 h-60 mx-4 rounded border border-blue-200 border-opacity-30 flex flex-col align-top items-center justify-start">
-                        <img src="{{asset('img/bgd1.png')}}" alt="" class="w-full h-5/6 rounded mb-2 mt-0">
-                        <div class="mx-2 text-center text-xl text-stone-500 font-semibold capitalize">Product1</div>
+                    <div class="w-60 h-60 mx-4 rounded border-y-2 bg-p border-s border-double border-opacity-30 flex flex-col align-top items-center justify-start">
+                        <img src="{{asset('img/sunny.jpg')}}" alt="" class="w-full h-5/6 rounded mb-2 mt-0">
+                        <div class="mx-2 text-center text-xl text-s font-semibold capitalize">Product1</div>
                     </div>
-                    <div class="w-60 h-60 mx-4 rounded border border-blue-200 border-opacity-30 flex flex-col align-top items-center justify-start">
-                        <img src="{{asset('img/bgd1.png')}}" alt="" class="w-full h-5/6 rounded mb-2 mt-0">
-                        <div class="mx-2 text-center text-xl text-stone-500 font-semibold capitalize">Product1</div>
+                    <div class="w-60 h-60 mx-4 rounded border-y-2 bg-p border-s border-double border-opacity-30 flex flex-col align-top items-center justify-start">
+                        <img src="{{asset('img/sunny.jpg')}}" alt="" class="w-full h-5/6 rounded mb-2 mt-0">
+                        <div class="mx-2 text-center text-xl text-s font-semibold capitalize">Product1</div>
                     </div>
-                    <div class="w-60 h-60 mx-4 rounded border border-blue-200 border-opacity-30 flex flex-col align-top items-center justify-start">
-                        <img src="{{asset('img/bgd1.png')}}" alt="" class="w-full h-5/6 rounded mb-2 mt-0">
-                        <div class="mx-2 text-center text-xl text-stone-500 font-semibold capitalize">Product1</div>
+                    <div class="w-60 h-60 mx-4 rounded border-y-2 bg-p border-s border-double border-opacity-30 flex flex-col align-top items-center justify-start">
+                        <img src="{{asset('img/sunny.jpg')}}" alt="" class="w-full h-5/6 rounded mb-2 mt-0">
+                        <div class="mx-2 text-center text-xl text-s font-semibold capitalize">Product1</div>
                     </div>
                 </div>
             </div>
--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -1,22 +1,22 @@
-<div class="w-full bg-white">
+<div class="w-full bg-d">
     <div class="flex">
-        <div class="flex-1 flex items-center justify-start py-3"><span class="text-2xl text-blue-500 mx-6 font-black px-1" title="more about sonsman">&iiint;</span></div>
+        <div class="flex-1 flex items-center justify-start py-3"><span class="text-2xl text-p mx-6 font-black px-1" title="more about sonsman">&iiint;</span></div>
         <div class="flex-1 flex items-center justify-around py-3">
-            <span class="text-2xl font-semibold text-blue-500 italic">&phone; +237672908239 &Ffr; germanuskeming@gmail.com</span>
-            <a href="{{url('/sign-in')}}" class="text-2xl capitalize text-blue-500 rounded-full font-semibold px-1" title="sign in">&VDash;</a>
+            <span class="text-2xl font-semibold text-p italic">&phone; +237672908239 &Ffr; germanuskeming@gmail.com</span>
+            <a href="{{url('/sign-in')}}" class="text-2xl capitalize text-p rounded-full font-semibold px-1" title="sign in">&VDash;</a>
         </div>
     </div>
-    <div class="flex bg-blue-500 justify-between">
+    <div class="flex bg-p justify-between">
         <div class=" w-1/3 py-3 justify-start px-6">
-            <a href="{{url('/')}}" class="text-blue-800 text-4xl font-black font-serif tracking-tighter border-2 px-3 shadow-inner bg-gradient-to-r via-blue-400 from-white to-white shadow-blue-500 rounded-r-full rounded-l-full uppercase">sonsman</a>
+            <a href="{{url('/')}}" class="text-d text-3xl font-black font-serif tracking-tighter border-2 border-d px-3 shadow-inner bg-p  rounded-r-full rounded-l-full uppercase">sonsman</a>
         </div>
         <div class="w-2/3 justify-center flex px-4 py-3 items-center align-middle text-xl font-semibold font-serif">
-            <a href="{{url('/home')}}" class="block no-underline text-white px-w capitalize px-3">Home</a>
-            <a href="{{url('/services')}}" class="block no-underline text-white px-w capitalize px-3">services</a>
-            <a href="{{url('/works')}}" class="block no-underline text-white px-w capitalize px-3">works</a>
-            <a href="{{url('/quotes')}}" class="block no-underline text-white px-w capitalize px-3">s-quotes</a>
-            <a href="{{url('/blog')}}" class="block no-underline text-white px-w capitalize px-3">s-blog</a>
-            <a href="{{url('/help')}}" class="block no-underline text-white px-w capitalize px-3">help</a>
+            <a href="{{url('/home')}}" class="block no-underline text-d px-w capitalize px-3">Home</a>
+            <a href="{{url('/services')}}" class="block no-underline text-d px-w capitalize px-3">services</a>
+            <a href="{{url('/works')}}" class="block no-underline text-d px-w capitalize px-3">works</a>
+            <a href="{{url('/quotes')}}" class="block no-underline text-d px-w capitalize px-3">s-quotes</a>
+            <a href="{{url('/blog')}}" class="block no-underline text-d px-w capitalize px-3">s-blog</a>
+            <a href="{{url('/help')}}" class="block no-underline text-d px-w capitalize px-3">help</a>
         </div>
     </div>
 </div>
